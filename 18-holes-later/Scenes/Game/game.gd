5529173f20c7e0f7424138109cc0fe49003467a8
tohzt <steven.g.french@live.com>
2024-07-27 03:10:06 -0600

    added disc bag

--- 18-holes-later/Scenes/Game/game.gd
+++ 18-holes-later/Scenes/Game/game.gd
@@ -1,15 +1,17 @@
 extends Node3D
 
+const CLUBHOUSE = "res://Scenes/Clubhouse/Clubhouse.tscn"
+
 var Player : CharacterBody3D
 var Cam : SpringArm3D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	Player = get_node("Player")
 	Cam = Player.get_node("SpringArm3D")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func go_to_clubhouse():
+	get_tree().change_scene_to_file(CLUBHOUSE)
--- 18-holes-later/global.gd
+++ 18-holes-later/global.gd
@@ -1,22 +1,32 @@
 extends Node
 
 var MOUSE_SENSITIVITY : float = 0.4
+const GAME_SCENE = "res://Scenes/Game/game.tscn"
 const DISC = preload("res://Objects/Disc/disc.tscn")
 var Player : CharacterBody3D
 var Scene : Node
+#var Last_Hole = "res://Scenes/Holes/Hole_01/hole_01.tscn"
 
-var debug_stats = [13,5,-1,3]
+var game_disc_index: int
+var Bag: Array
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	Scene = get_tree().root.get_child(1)
 
 func _process(_delta):
+	if Input.is_action_just_pressed("tab"):
+		game_disc_index += 1
+		if game_disc_index >= Bag.size():
+			game_disc_index = 0
+		check_bag()
+	
 	if !Player: 
 		Player = get_tree().get_first_node_in_group("Player")
 		if  Player:
 			Scene = get_tree().root.get_child(1)
 	else: return
+	
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -24,4 +34,12 @@ func _unhandled_input(_event):
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE 
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+
+func add_disc(_disc):
+	Bag.append(_disc)
+	check_bag()
+
+func check_bag():
+	print("Bag Content: ", Bag)
+
+
