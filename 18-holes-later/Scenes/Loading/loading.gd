b73758ea02f37f50393263500f7fef37885c58c2
tohzt <steven.g.french@live.com>
2024-07-27 22:37:47 -0600

    added Loading scene transition and basic menu layout

---
+++ 18-holes-later/Scenes/Loading/loading.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+var loading_status : int
+var progress : Array[float]
+
+@onready var progress_bar : ProgressBar = $ProgressBar
+
+func _ready() -> void:
+	printt("Loading... ", Global.scene_current)
+	# Request to load the target scene:
+	ResourceLoader.load_threaded_request(Global.scene_current)
+	
+func _process(_delta: float) -> void:
+	# Update the status:
+	loading_status = ResourceLoader.load_threaded_get_status(Global.scene_current, progress)
+	
+	# Check the loading status:
+	match loading_status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			progress_bar.value = progress[0] * 100 # Change the ProgressBar value
+		ResourceLoader.THREAD_LOAD_LOADED:
+			# When done loading, change to the target scene:
+			get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(Global.scene_current))
+		ResourceLoader.THREAD_LOAD_FAILED:
+			# Well some error happend:
+			print_debug("Error. Could not load Resource")
--- 18-holes-later/Scenes/Main_Menu/main_menu.gd
+++ 18-holes-later/Scenes/Main_Menu/main_menu.gd
@@ -1,11 +1,31 @@
 extends Control
-const GAME = "res://Scenes/Clubhouse/Clubhouse.tscn"
+@onready var options_container = $"OptionsContainer"
+@onready var profile_container = $ProfileContainer
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	options_container.hide()
 
-func _on_btn_start_pressed():
-	get_tree().change_scene_to_file(GAME)
+
+func _on_btn_new_game_pressed():
+	Global.go_to_scene(Global.SCENE_CLUBHOUSE)
+
+func _on_btn_continue_pressed():
+	pass
+
+func _on_btn_load_pressed():
+	if profile_container.visible:
+		profile_container.hide()
+	else:
+		options_container.hide()
+		profile_container.show()
+
+func _on_btn_options_pressed():
+	if options_container.visible:
+		options_container.hide()
+	else:
+		profile_container.hide()
+		options_container.show()
+
 
 func _on_btn_exit_pressed():
 	get_tree().quit()
