5e9ca0937276bde573ee4bab9e13b8acded2fa10
tohzt <steven.g.french@live.com>
2024-07-30 01:09:09 -0600

    refactored InputController, Course Structure, Global Structure, Player Spawn

---
+++ 18-holes-later/Scenes/Holes/Hole_02/hole_02.gd
@@ -0,0 +1,14 @@
+extends Node3D
+@onready var trees = $Trees
+@onready var basket = $Basket
+@onready var discs = $Discs
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
--- 18-holes-later/Scenes/Pause_Menu/pause_menu.gd
+++ 18-holes-later/Scenes/Pause_Menu/pause_menu.gd
@@ -9,12 +9,13 @@ func _process(_delta):
 		show()
 
 func _on_btn_exit_pressed():
-	# WARNING: This couls close before save is complete 
-	Global.Scene.save_game()
+	# WARNING: This could close before save is complete 
+	#get_parent().save_game()
 	get_tree().quit()
 
 func _on_btn_resume_pressed():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Global.is_paused = false
 
 
 func _on_btn_retry_pressed():
