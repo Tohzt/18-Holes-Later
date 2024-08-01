20080d3c8497f69c7d7e2077b95fd5f6f6f95c0b
Dylan James <alazyartist@gmail.com>
2024-07-31 23:33:18 -0600

    Resource Editor

--- 18-holes-later/Objects/Shards/Shards/shard.gd
+++ 18-holes-later/Objects/Shards/Shards/shard.gd
@@ -2,10 +2,12 @@ class_name Shard
 extends Resource
 enum shard_types {Player,Enemy,Weapon}
 enum shard_rarity {Bone,Charred,Titanium,Special}
+enum perks {health,critical_hit,movement_speed,damage_over_time,explosion,multiply_projectiles,fire_rate}
 @export var name:String
-@export var type:shard_types
+@export var activation_type:shard_types
+@export var intensity: int
+@export var perk: perks
 @export var rarity:shard_rarity
-@export var master_shard:Array[Shard]
 
 
 
--- 18-holes-later/Objects/Shards/master_shard.gd
+++
@@ -1,6 +0,0 @@
-class_name master_shard
-extends Resource
-
-@export var name:String
-@export var special: String
-@export var activating_shards: Array[]
