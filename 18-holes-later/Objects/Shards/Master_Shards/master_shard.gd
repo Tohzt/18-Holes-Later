09f61927e7d0b0c1c7265accb13ddd84e905e74b
RadiantUnderground <towels2110@gmail.com>
2024-08-01 18:07:40 -0700

    Shards Stats

--- 18-holes-later/Objects/Shards/Master_Shards/master_shard.gd
+++ 18-holes-later/Objects/Shards/Master_Shards/master_shard.gd
@@ -4,3 +4,4 @@ enum element_type {Earth,Wind,Fire,Water,Lightning,Metal,Holy,Dark}
 @export var name:String
 @export var elemntal: element_type
 @export var activating_shards: Array[Shard_Special]
+@export var note:String
--- 18-holes-later/Objects/Shards/Shards/shard.gd
+++ 18-holes-later/Objects/Shards/Shards/shard.gd
@@ -1,14 +1,14 @@
 class_name Shard
 extends Resource
 enum shard_types {Player,Enemy,Weapon}
-enum shard_rarity {Bone,Charred,Titanium,Special}
-enum perks {health,critical_hit,movement_speed,damage_over_time,explosion,multiply_projectiles,fire_rate}
+enum shard_rarity {Bone,Charred,Titanium,Graphene}
+enum perks {health,disc_scale,critical_hit,movement_speed,damage_over_time,explosion,multiply_projectiles,fire_rate}
 @export var name:String
 @export var activation_type:shard_types
 @export var intensity: int
 @export var perk: perks
 @export var rarity:shard_rarity
-
+@export var note:String
 
 
 # Called when the node enters the scene tree for the first time.
