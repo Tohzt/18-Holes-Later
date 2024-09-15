040a088a49a4b2120208a043b7b96a748948553d
tohzt <steven.g.french@live.com>
2024-09-14 18:44:38 -0600

    added laker disx

--- 18-holes-later/Objects/Disc/Mesh/disc_design.gd
+++ 18-holes-later/Objects/Disc/Mesh/disc_design.gd
@@ -1,20 +1,5 @@
 extends MeshInstance3D
 
-#func _ready():
-	## Check if the mesh already has a material
-	#var material = get_surface_override_material(0)
-	#
-	## If it doesn't, create a new StandardMaterial3D
-	#if material == null:
-		#material = StandardMaterial3D.new()
-		#set_surface_override_material(0, material)
-	#
-	## Generate a random color
-	#var random_color = Color(randf(), randf(), randf())
-	#
-	## Set the albedo color of the material
-	#material.albedo_color = random_color
-
 func _process(_delta):
 	# TODO: Optimize
 	var material: Material = mesh.surface_get_material(0)
