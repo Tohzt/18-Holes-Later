ff0d67875858bfd4fa90bdc818b4d1924ce7ac4c
tohzt <steven.g.french@live.com>
2024-09-18 00:32:23 -0600

    tried to add barrel

--- 18-holes-later/Objects/Trees/tree.gd
+++ 18-holes-later/Objects/Trees/tree.gd
@@ -1,16 +1,4 @@
 extends StaticBody3D
 
-@onready var sprite_3d = $Sprite3D
-
 func _ready():
-	scale *= [0.75, 1, 0.5].pick_random()
-	# Ensure the sprite has a material override
-	if sprite_3d.material_override == null:
-		sprite_3d.material_override = ShaderMaterial.new()
-	# Assign the shader to the material override
-	#sprite_3d.material_override.shader = load("res://Objects/Trees/tree_shake.gdshader")
-
-
-func trigger_wiggle():
-	# Reset the time offset to current time to restart the wiggle
-	sprite_3d.material_override.set_shader_parameter("time_offset", Time.get_ticks_msec() / 1000.0)
+	scale *= [0.75, 0.4, 0.5].pick_random()
