4ef0501574941aba909452d664ad0c0dd7c8faea
tohzt <steven.g.french@live.com>
2024-09-21 16:00:15 -0600

    using lerp_angle for smooth camera rotation

--- 18-holes-later/Entities/_Controllers/InputControllers/Vehicle/golf_cart_input.gd
+++ 18-holes-later/Entities/_Controllers/InputControllers/Vehicle/golf_cart_input.gd
@@ -4,20 +4,20 @@ var turn_strength = 0.0
 var max_turn_strength = 100.0
 
 func _handle_input(delta = 0.0):
-	_cart_mouse_input()
-	_cart_movement_input(delta)
+	cart_mouse_input()
+	cart_movement_input(delta)
 
 func _input(event):
 	mouse_motion = null
 	if event is InputEventMouseMotion:
 		mouse_motion = event
 
-func _cart_mouse_input():
+func cart_mouse_input():
 	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE: return
 	if mouse_motion:
 		Master.rotate_y(deg_to_rad(-mouse_motion.relative.x * Global.Settings.MOUSE_SENSITIVITY))
 
-func _cart_movement_input(delta):
+func cart_movement_input(delta):
 	var input_forward = Input.get_axis("ui_up", "ui_down")
 	var input_turn = Input.get_axis("ui_left", "ui_right")
 	turn_strength = lerp(turn_strength,max_turn_strength,delta) * abs(input_turn)
--- 18-holes-later/Entities/_Controllers/InputControllers/input_controller.gd
+++ 18-holes-later/Entities/_Controllers/InputControllers/input_controller.gd
@@ -3,7 +3,8 @@ extends Node
 
 @onready var Master: Node3D = $".."
 var mouse_motion = null
-var mouse_velocity = Vector3.ZERO
+var target_rotation = 0.0
+var rotation_speed = 5
 
 func _process(delta):
 	if Master.accepts_input: _handle_input(delta)
@@ -11,12 +12,28 @@ func _process(delta):
 func _handle_input(_delta): 
 	print_debug("_handle_input not set")
 
-func _char_mouse_input():
-	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE: return
+func char_mouse_input(delta):
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		return
 	if mouse_motion:
-		Master.rotate_y(deg_to_rad(-mouse_motion.relative.x * Global.Settings.MOUSE_SENSITIVITY))
+		# Update the target rotation based on mouse input
+		var turn_limit = clamp(mouse_motion.relative.x,-5,5)
+		target_rotation -= turn_limit
+		
+		# Ensure target_rotation stays within 0-360 range
+		target_rotation = fmod(target_rotation, 360.0)
+		if target_rotation < 0:
+			target_rotation += 360.0
+		
+		# Use lerp_angle for smooth rotation
+		var current_rotation = fmod(Master.rotation.y, TAU)
+		var new_rotation = deg_to_rad(target_rotation)
+		
+		# Apply the new rotation
+		Master.rotation.y = lerp_angle(Master.rotation.y,new_rotation,delta*rotation_speed)
+		printt(rad_to_deg(Master.rotation.y), rad_to_deg(new_rotation))
 
-func _char_movement_input():
+func char_movement_input():
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	#Master.input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	Master.input_dir = (Master.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
