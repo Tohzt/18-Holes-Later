dfdc509484c95231f2e397c762b43d52030a0850
tohzt <steven.g.french@live.com>
2024-10-13 17:22:36 -0600

    worked on main menu a bit

--- 18-holes-later/18-holes-later/Objects/vehicles/Disc Launcher/disc_launcher.gd
+++ 18-holes-later/18-holes-later/Objects/vehicles/Disc Launcher/disc_launcher.gd
@@ -5,6 +5,7 @@ extends Node3D
 @onready var Barrel_Pivot = $"Barrel Pivot"
 @onready var Barrel_Exit = $"Barrel Pivot/Barrel Exit"
 @onready var Barrel_Dir: RayCast3D = $"Barrel Pivot/RayCast3D"
+@onready var Detect_Player = $"Detect Player"
 @onready var Input_Controller: InputController = $InputController
 
 var accepts_input = false
@@ -20,16 +21,27 @@ func _ready():
 	set_active(false)
 	
 func _process(_delta):
-	if !accepts_input: return
+	if !accepts_input: 
+		var overlap = Detect_Player.get_overlapping_bodies()
+		if overlap: _detect(overlap)
+		return
+	
 	
 	new_dir.y = Input_Controller.rot_cam.y
-	
 	Barrel_Pivot.rotation.y = new_dir.y
 	
 	if did_shoot:
 		did_shoot = false
 		throw_disc()
-		print("FIRE!")
+
+func _detect(overlap):
+	for body in overlap:
+		if body.is_in_group("Character"):
+			if body.did_interact:
+				if !accepts_input:
+					set_active(true)
+					Global.Player.set_active(false)
+					Global.Cameraman.set_target(self, Cam_Focus)
 
 func set_active(TorF: bool):
 	if TorF:
@@ -49,8 +61,6 @@ func throw_disc():
 	var ammo: Disc = ammo_rig.instantiate()
 	ammo.position = Barrel_Exit.global_position
 	ammo.target_dir = Barrel_Dir.global_transform.basis * -Barrel_Dir.target_position.normalized()
-	#ammo.target_dir = Barrel_Pivot.position.direction_to(Barrel_Exit.position)
-	#ammo.target_dir = Global.Cameraman.Camera.get_global_transform().basis.z
 	ammo.power = 10
 	ammo.in_bag = false
 	ammo.in_hand = false
