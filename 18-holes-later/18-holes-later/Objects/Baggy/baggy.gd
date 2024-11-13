15fbc90ad9cc35904995260d21130657d0a7d3dc
tohzt <steven.g.french@live.com>
2024-11-12 21:13:25 -0700

    target cycle success

--- 18-holes-later/18-holes-later/Objects/Baggy/baggy.gd
+++ 18-holes-later/18-holes-later/Objects/Baggy/baggy.gd
@@ -4,9 +4,6 @@ extends Entity
 var rotation_speed := 5.0
 var targets_in_range: Array[Node3D]
 
-func _physics_process(delta):
-	pass
-
 func rotate_to_target(delta):
 	var direction = Target.global_position - global_position
 	if direction.is_zero_approx(): return
